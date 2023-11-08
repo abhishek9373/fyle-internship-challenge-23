00173af543dd07cc57cb476885bd9a9335b84447
= <=>
2023-11-08 23:48:33 +0530

    user component ui done

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'src/app/core/interfaces/User.interface';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit{
+export class UserComponent implements OnInit {
 
-  constructor(private router: Router){ }
+  user!: User;
+  userName!: string;
+
+  constructor(private router: Router) {
+    try {
+      
+    } catch (error: any) {
+      throw (error);
+    }
+  }
 
   ngOnInit(): void {
 
   }
-  
+
 }
