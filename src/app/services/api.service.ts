94160107ff66712e59acb306dcf5773ba97ec8cd
= <=>
2023-11-10 00:14:42 +0530

    api services old removed

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -11,9 +11,5 @@ export class ApiService {
     private httpClient: HttpClient
   ) { }
 
-  getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
-  }
 
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
 }
