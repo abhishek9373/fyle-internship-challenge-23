7572c9b582f8fa784f7c338c607a03bbc390df80
= <=>
2023-11-09 23:44:43 +0530

    simplified controller using ngx-pagination

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Repo } from 'src/app/core/interfaces/Repo.interface';
 import { User } from 'src/app/core/interfaces/User.interface';
 import { UserRepositoryService } from 'src/app/core/services/user-repository.service';
@@ -14,101 +14,77 @@ export class UserComponent implements OnInit {
 
   user!: User;
   userName!: string;
-  userRepoCount!: number;
-  repos!: Array<Repo> | [];
+  repos!: Repo[] | [];
   page: number = 1;
-  perPage: number = 10;
-  totalPages!: number;
-  pageArray!: Array<number>;
+  perPage: number = 12;
+  totalCount!: number;
+  totalPages: number = 1;
+  currentPageArray: number[] = [];
+  loading: boolean = true;
 
-  constructor(private router: Router, private activatedRoutes: ActivatedRoute, private userService: UserService, private userRepoService: UserRepositoryService) { }
+  constructor(
+    private router: Router,
+    private activatedRoutes: ActivatedRoute,
+    private userService: UserService,
+    private userRepoService: UserRepositoryService
+  ) {}
 
   ngOnInit() {
-    try {
-      // get userName from params
-      this.activatedRoutes.params.subscribe((params: any) => {
-        if (params && params['userName']) {
-          this.userName = params['userName'];
-          this.userService.getUserByUserName(this.userName).subscribe((data: any) => {
-            if (data === null) {
-              window.alert('User not found');
-            } else {
-              this.user = data;
-              this.userRepoCount = this.user.public_repos;
-              // update pagination array
-              this.totalPages = Math.ceil(this.userRepoCount / this.perPage);
-              const count: number = (this.totalPages > 9) ? 9 : this.totalPages;
-              this.pageArray = Array(count).fill(0).map((x, i) => i);
-            }
-          });
-        }
-      });
-
-      // get page from queryparams
-      this.activatedRoutes.queryParams.subscribe((params: any)=>{
-        if(params && params['page']){
-          this.page=parseInt(params['page']);
-        }else{
-          this.page=1;
-        }
-        if(params['per_page']){
-          this.perPage = parseInt(params.per_page);
-          this.totalPages = Math.ceil(this.userRepoCount / this.perPage);
-          const count: number = (this.totalPages > 9) ? 9 : this.totalPages;
-          this.pageArray = Array(count).fill(0).map((x, i) => i);
-        }
-      });
-
-      // get repo list of the user by username and pagination
-      this.userRepoService.getRepositoriesOfTheUser(this.userName, this.page, this.perPage | 12).subscribe((repos: Array<Repo>)=>{
-        this.repos=repos;
-      });
-
-    } catch (error: any) {
-      window.alert("User not found");
-    }
-  }
-
-  changePage(newPage: number){
-    try{
-      // update repos as per the page no
-      // skip if pageno is same as current page no
-      if(this.page === newPage){
-        return;
+    this.activatedRoutes.params.subscribe((params: any) => {
+      if (params && params['userName']) {
+        this.userName = params['userName'];
+        this.userService.getUserByUserName(this.userName).subscribe((data: any) => {
+          if (data === null) {
+            window.alert('User not found');
+          } else {
+            this.user = data;
+            this.totalCount = this.user.public_repos;
+            this.totalPages = Math.ceil(this.totalCount / this.perPage);
+            this.generateCurrentPageArray();
+            this.fetchRepositories();
+          }
+        });
       }
-      const currentParams = { ...this.activatedRoutes.snapshot.queryParams };
-      currentParams['page'] = newPage;
-      this.router.navigate([], {
-        relativeTo: this.activatedRoutes,
-        queryParams: currentParams,
-        queryParamsHandling: 'merge'
-      })
-      this.page = newPage;
-      this.userRepoService.getRepositoriesOfTheUser(this.userName, this.page, this.perPage | 12).subscribe((data: Array<Repo>)=>{
-        this.repos = data;
-      })
+    });
 
-    }catch(error){
-      throw(error);
+    this.activatedRoutes.queryParams.subscribe((params: any) => {
+      if (params && params['page']) {
+        this.page = parseInt(params['page']);
+      } else {
+        this.page = 1;
+      }
+      if (params['per_page']) {
+        this.perPage = parseInt(params.per_page);
+      }
+    });
+  }
+
+  changePage(newPage: number) {
+    if (this.page !== newPage) {
+      this.page = newPage;
+      this.generateCurrentPageArray();
+      this.fetchRepositories();
     }
   }
 
-  goBack(){
+  goBack() {
     this.router.navigate(['']);
   }
 
-  // change page by previous and next button
-
-  prevPage(){
-    if(this.page > 1){
-      this.changePage(this.page-1);
-    }
+  private fetchRepositories() {
+    this.loading = true; // Set loading to true when fetching repos
+    this.userRepoService
+      .getRepositoriesOfTheUser(this.userName, this.page, this.perPage)
+      .subscribe((repos: Repo[]) => {
+        this.repos = repos;
+        this.loading = false; // Set loading to false after fetching repos
+      });
   }
 
-  nextPage(){
-    if(this.page < this.totalPages){
-      this.changePage(this.page+1);
-    }
-  }
+  generateCurrentPageArray() {
+    const start = Math.max(1, this.page - 2);
+    const end = Math.min(this.totalPages, this.page + 2);
 
+    this.currentPageArray = Array.from({ length: end - start + 1 }, (_, i) => start + i);
+  }
 }
