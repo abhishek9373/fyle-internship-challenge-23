ff24a1bb35b25b121f824d3653417f6b3969f793
= <=>
2023-11-10 14:41:46 +0530

    Update repo service for repo languages

--- src/app/core/services/user-repository.service.ts
+++ src/app/core/services/user-repository.service.ts
@@ -1,17 +1,44 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable, forkJoin } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { Repo } from '../interfaces/Repo.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserRepositoryService {
+  private apiUrl = 'https://api.github.com';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getRepositoriesOfTheUser(userName: string, page: number | 1, perPage: number | 10): Observable<Array<Repo>>{
-    return this.http.get<Array<Repo>>(`https://api.github.com/users/${userName}/repos?page=${page}&per_page=${perPage}`);
+  getRepositoriesOfTheUser(username: string, page: number, perPage: number): Observable<Repo[]> {
+    const url = `${this.apiUrl}/users/${username}/repos?page=${page}&per_page=${perPage}`;
+
+    return this.http.get<Repo[]>(url);
   }
 
+  getRepoLanguages(languagesUrl: string): Observable<any> {
+    return this.http.get<any>(languagesUrl);
+  }
+
+  getRepositoriesWithLanguages(username: string, page: number, perPage: number): Observable<Repo[]> {
+    // github blocks if rate limit exceeds 5 so check perpage is smaller than 5
+    if(perPage>5){
+      return this.getRepositoriesOfTheUser(username, page, perPage);
+    }
+    return this.getRepositoriesOfTheUser(username, page, perPage).pipe(
+      switchMap((repos) => {
+        const repoLanguageRequests = repos.map(repo => this.getRepoLanguages(repo.languages_url));
+        return forkJoin(repoLanguageRequests).pipe(
+          map(languagesArray => {
+            repos.forEach((repo, index) => {
+              repo.languages = Object.keys(languagesArray[index]);
+            });
+            return repos;
+          })
+        );
+      })
+    );
+  }
 }
